c7c3c21cca0faae6fc404148bc87cfc4694870bf
YuminPark <p353700@gmail.com>
2026-04-09 17:48:58 +0900

    refactor: .svgrrc.json 파일 수정

--- packages/icons/src/generated/index.ts
+++ packages/icons/src/generated/index.ts
@@ -1,4 +1,3 @@
 export { default as ArrowRightIcon } from "./ArrowRightIcon"
 export { default as CheckIcon } from "./CheckIcon"
 export { default as HomeIcon } from "./HomeIcon"
-export { default as KusitmsIcon } from "./KusitmsIcon"
