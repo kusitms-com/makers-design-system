82a699b0b4d61d00d1f412d4dfb69072b333cbfe
YuminPark <p353700@gmail.com>
2026-04-09 17:52:59 +0900

    fix: svgr 설정 파일 단일화 및 viewBox 유지

--- packages/icons/.svgrrc.cjs
+++ packages/icons/.svgrrc.cjs
@@ -1,28 +1,15 @@
 const path = require("path")
 
-const template = (variables, { tpl }) => {
-  const componentName = variables.componentName.replace(/^Svg/, "")
-  return tpl`
-${variables.imports}
-export default function ${componentName}(${variables.props}) {
-  return ${variables.jsx}
-}
-`
-}
-
 const indexTemplate = (filePaths) => {
   const entries = filePaths.map(({ path: filePath }) => {
     const basename = path.basename(filePath, path.extname(filePath))
-    const pascalName = basename
-      .split("-")
-      .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
-      .join("")
-    return `export { default as ${pascalName} } from "./${basename}"`
+    return `export { default as ${basename} } from "./${basename}"`
   })
   return entries.join("\n") + "\n"
 }
 
 module.exports = {
+  jsxRuntime: "automatic",
   typescript: true,
   icon: true,
   expandProps: "end",
@@ -34,6 +21,5 @@ module.exports = {
     "#000000": "currentColor",
     black: "currentColor",
   },
-  template,
   indexTemplate,
 }
